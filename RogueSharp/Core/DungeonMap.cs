61b5e20b61391d8cb8a3bb294ceb65688257e282
Josh Jacobsen <joshjacobsen5@gmail.com>
2017-01-03 21:20:24 +1300

    set up MapGenerator

--- RogueSharp/Core/DungeonMap.cs
+++ RogueSharp/Core/DungeonMap.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using RogueSharp;
 using RLNET;
 
 namespace RogueSharp.Core
@@ -17,7 +18,7 @@ namespace RogueSharp.Core
             }
         }
 
-        private void SetConsoleSymbolForCell(RLConsole mapConsole, Cell cell)
+        private void SetConsoleSymbolForCell(RLConsole console, Cell cell)
         {
             // If cell not explored, don't draw anything
             if(!cell.IsExplored)
@@ -29,9 +30,30 @@ namespace RogueSharp.Core
             // colors
             if (IsInFov(cell.X, cell.Y))
             {
-                // Choose the symbol to use if the cell is walkable or not
-                // "." for floor and "#" for walls
+                // Choose symbol to draw based on if cell walkable or not
+                // '.' for floor and '#' for walls
+                if (cell.IsWalkable)
+                {
+                    console.Set(cell.X, cell.Y, Colors.FloorFov, Colors.FloorBackgroundFov, '.');
+                }
+                else
+                {
+                    console.Set(cell.X, cell.Y, Colors.WallFov, Colors.WallBackgroundFov, '#');
+                }
             }
+            // when cell is outside FOV, draw with darker colors
+            else
+            {
+                if (cell.IsWalkable)
+                {
+                    console.Set(cell.X, cell.Y, Colors.Floor, Colors.FloorBackground, '.');
+                }
+                else
+                {
+                    console.Set(cell.X, cell.Y, Colors.Wall, Colors.WallBackground, '#');
+                }
+            }
+
         }
     }
 }
